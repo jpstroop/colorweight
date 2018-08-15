7f2b7ea8e5cbb2dde961838a89cb79ebf708174c
Jon Stroop <jpstroop@gmail.com>
2018-08-15 18:28:48 -0400

    Adds weighted viz

--- utils/color_analysis.py
+++ utils/color_analysis.py
@@ -37,58 +37,64 @@ class ImageAnalyzer(object):
         # How often does each unique label occur?
         #    returns (array(labels), array(frequency))
         label_freq = np.unique(labels, return_counts=True)
-
         # Zip the two arrays above into a list: [(label, freq),]
         label_freq_pairs = zip(label_freq[0], label_freq[1])
-
         # Sort the pairs by frequency, descending
         freq_sorted = sorted(label_freq_pairs, key=lambda t: t[1], reverse=True)
-
         # Convert the members of the centroids to ints
         palette = np.uint8(centroids)
-
         # Replace the labels with the value from the palette/centroids
         # giving us: [([B,G,R], freq), ([B,G,R], freq), ...]
         pallet_by_freq = [(palette[f[0]], f[1]) for f in freq_sorted]
 
         return pallet_by_freq
 
-    # def viz(self, n_colors=5, height=100, width=200):
-    #     # See: https://stackoverflow.com/a/12890573/714478
-    #     colors = self.dominant_colors(n_colors)
-    #     image = np.zeros((height, width, 3), np.uint8)
-    #     d = 1/len(colors)
-    #     for i in range(len(colors)):
-    #         print(str(int(i*d*width)) + " : " + str(colors[i]))
-    #         image[:, int(i*d*width):width] = colors[i]
-    #     cv2.imshow('image', image)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
-
-    def viz(self, n_colors=5, height=100, width=400, weighted=True):
+    def viz(self, n_colors=5, height=100, width=400, weighted=True, debug=False):
         # See: https://stackoverflow.com/a/12890573/714478
         colors = self.dominant_colors(n_colors)
         image = np.zeros((height, width, 3), np.uint8)
         if weighted:
-            pass
+            image = ImageAnalyzer._viz_weighted(image, colors, width, debug)
         else:
-            image = ImageAnalyzer._viz_unweighted(image, colors, width)
-        cv2.imshow('image', image)
+            image = ImageAnalyzer._viz_unweighted(image, colors, width, debug)
+        ImageAnalyzer._show(image)
+
+    @staticmethod
+    def _show(image, label='[Image]'):
+        cv2.imshow(label, image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     @staticmethod
-    def _viz_unweighted(image, colors, width):
+    def _viz_weighted(image, colors, width, debug=False):
+        total_pixels = sum([t[1] for t in colors])
+        offset = 0
+        for i in range(len(colors)):
+            if i == len(colors)-1:
+                # Hack because we can be off (by 1, more?) in the last pass
+                # because fractions and pixels...
+                section_width = width - offset
+            else:
+                section_width = int(colors[i][1] / total_pixels * width)
+            if debug:
+                stmt = 'BGR: {}, Offset: {}, Section Width: {}'
+                print(stmt.format(colors[i][0], section_width, offset))
+            image[:, offset:width] = colors[i][0]
+            offset = offset + section_width
+        return image
+
+    @staticmethod
+    def _viz_unweighted(image, colors, width, debug=False):
         section_width = 1/len(colors)
         for i in range(len(colors)):
             offset = int(i*section_width*width)
-            print(str(offset) + " : " + str(colors[i]))
+            if debug:
+                print('BGR: {}, Offset: {}'.format(colors[i][0], offset))
             image[:, offset:width] = colors[i][0]
         return image
-
 
 # TODO: should be a param arg
 IMAGE_PATH = '/Users/jstroop/workspace/colorfun/images/0034.jpg'
 if __name__ == '__main__':
     analyzer = ImageAnalyzer(IMAGE_PATH)
-    print(analyzer.viz(weighted=False))
+    analyzer.viz()
