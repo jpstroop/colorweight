f134ef8ebd1e64e97f1acd6514fe412c97df57c2
Jon Stroop <jpstroop@gmail.com>
2018-09-09 12:28:06 -0400

    incorporate smart K stuff into color_analysis

--- cw/utils/color_analysis.py
+++ cw/utils/color_analysis.py
@@ -1,17 +1,23 @@
+from numpy.matlib import repmat
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-from json import dumps
-
 #
 # Pass the path to an image to this script to visualize the dominant colors in
 # the image. See the viz() method for additional options.
 #
-DEFAULT_N_COLORS = 5
+
+CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, 1.0) # consider 1.0; may want lower?
+FLAGS = cv2.KMEANS_RANDOM_CENTERS
+K_MAX = 12
+DEBUG = False
 
 class ImageAnalyzer(object):
     def __init__(self, image_path):
         self.image_path = image_path
         self._image_data = None
+        self._pixels = None
+        self._cluster_data = []
         self._average_color = None
 
     @property
@@ -21,19 +27,35 @@ class ImageAnalyzer(object):
         return self._image_data
 
     @property
+    def pixels(self):
+        if self._pixels is None:
+            arr = np.float32(self.image_data) # a 3d ndarray
+            self._pixels = arr.reshape((-1, 3))
+        return self._pixels
+
+    @property
     def average_color(self):
+        # useless
         if self._average_color is None:
             self._average_color = [self.image_data[:, :, i].mean()
                                     for i in range(self.image_data.shape[-1])]
         return self._average_color
 
-    def dominant_colors(self, n_colors=DEFAULT_N_COLORS):
+    @property
+    def cluster_data(self):
+        # runs through the image K_MAX times...can take a while!
+        if not self._cluster_data:
+            for k in range(1, K_MAX+1):
+                compactness, labels, centroids = self._k_means(k)
+                self._cluster_data.append((k, compactness, labels, centroids))
+        return self._cluster_data
+
+    def dominant_colors(self, n_colors=None):
         # See: https://docs.opencv.org/3.4.2/d1/d5c/tutorial_py_kmeans_opencv.html
-        arr = np.float32(self.image_data) # a 3d ndarray
-        pixels = arr.reshape((-1, 3))
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, 1.0)
-        flags = cv2.KMEANS_RANDOM_CENTERS
-        _, labels, centroids = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+        if n_colors is None:
+            _, _, labels, centroids = self._find_best_k()
+        else:
+            _, labels, centroids = self._k_means(n_colors)
 
         # "Labels will have the same size as that of test data where each data
         # will be labelled as '0','1','2' etc. depending on their centroids."
@@ -53,7 +75,7 @@ class ImageAnalyzer(object):
         # giving us: [([B,G,R], freq), ([B,G,R], freq), ...]
         return [(palette[f[0]], f[1]) for f in freq_sorted]
 
-    def viz(self, n_colors=DEFAULT_N_COLORS, height=100, width=400, weighted=True, debug=False):
+    def viz(self, n_colors=None, height=100, width=400, weighted=True, debug=DEBUG):
         # See: https://stackoverflow.com/a/12890573/714478
         colors = self.dominant_colors(n_colors)
         image = np.zeros((height, width, 3), np.uint8)
@@ -63,13 +85,59 @@ class ImageAnalyzer(object):
             pixels = ImageAnalyzer._viz_unweighted(image, colors, width, debug)
         return pixels
 
-    def list(self, n_colors=DEFAULT_N_COLORS):
+    def list(self, n_colors=None):
         return self._dominant_colors_list(n_colors=n_colors)
 
-    def _dominant_colors_list(self, n_colors=DEFAULT_N_COLORS):
+    def show_elbow(self):
+        ks = [e[0] for e in self.cluster_data]
+        dist = [e[1] for e in self.cluster_data]
+        plt.plot(ks, dist, 'bo-')
+        plt.plot((ks[0], ks[-1]), (dist[0], dist[-1]), 'r--')
+        best = self._find_best_k(self.cluster_data)
+        plt.plot((best[0]), (best[1]), 'gx', markersize=12, mew=4)
+        plt.xlabel('k')
+        plt.ylabel('distortion')
+        plt.title('elbow showing the optimal k')
+        plt.show()
+
+    def _dominant_colors_list(self, n_colors=None):
         colors = self.dominant_colors(n_colors=n_colors)
         total_pixels = sum([t[1] for t in colors])
         return [ImageAnalyzer._format_color_for_json(c, total_pixels) for c in colors]
+
+    def _k_means(self, k):
+        # returns (compactness, labels, centroids)
+        return cv2.kmeans(self.pixels, k, None, CRITERIA, 10, FLAGS)
+
+    def _find_best_k(self, debug=DEBUG):
+        # kd_data is [(k, dist, ...), (k, dist, ...), ...]
+        # See: https://en.wikipedia.org/wiki/Vector_projection
+        # and: https://stackoverflow.com/a/37121355/714478
+        dist_curve = [e[1] for e in self.cluster_data]
+        n_points = len(dist_curve) # TODO: use Ks instead
+        all_coords = np.vstack((range(n_points), dist_curve)).T
+        first_point = all_coords[0]
+        line_vec = all_coords[-1] - first_point
+        line_vec_norm = line_vec / np.sqrt(np.sum(line_vec**2))
+        vec_from_first = all_coords - first_point
+        scalar_prod = np.sum(vec_from_first * repmat(line_vec_norm, n_points, 1), axis=1)
+        vec_from_first_parallel = np.outer(scalar_prod, line_vec_norm)
+        vec_to_line = vec_from_first - vec_from_first_parallel
+        dist_to_line = np.sqrt(np.sum(vec_to_line ** 2, axis=1))
+        index_of_best_point = np.argmax(dist_to_line)
+        if debug:
+            print(f'Distortion curve: {dist_curve}')
+            print(f'All coords: {all_coords}')
+            print(f'First point: {first_point}')
+            print(f'Line vector: {line_vec}')
+            print(f'Line vector normalized: {line_vec_norm}')
+            print(f'Vector from first: {vec_from_first}')
+            print(f'Scalar product: {scalar_prod}')
+            print(f'Vector from first parallel: {vec_from_first_parallel}')
+            print(f'Vector to line: {vec_to_line}')
+            print(f'Distance to line: {dist_to_line}')
+            print(f'Index of best K: {index_of_best_point}')
+        return self.cluster_data[index_of_best_point]
 
     @staticmethod
     def _format_color_for_json(color_entry, total_pixels):
@@ -87,7 +155,7 @@ class ImageAnalyzer(object):
         cv2.destroyAllWindows()
 
     @staticmethod
-    def _viz_weighted(image, colors, width, debug=False):
+    def _viz_weighted(image, colors, width, debug=DEBUG):
         total_pixels = sum([t[1] for t in colors])
         offset = 0
         for i in range(len(colors)):
@@ -104,7 +172,7 @@ class ImageAnalyzer(object):
         return image
 
     @staticmethod
-    def _viz_unweighted(image, colors, width, debug=False):
+    def _viz_unweighted(image, colors, width, debug=DEBUG):
         section_width = 1/len(colors)
         for i in range(len(colors)):
             offset = int(i*section_width*width)
@@ -112,3 +180,12 @@ class ImageAnalyzer(object):
                 print(f'BGR: {colors[i][0]}, Offset: {offset}')
             image[:, offset:width] = colors[i][0]
         return image
+
+if __name__ == '__main__':
+    from cv2 import imwrite
+    image_path = '/Users/jstroop/workspace/colorweight/samples/01_in.jpg'
+    ia = ImageAnalyzer(image_path)
+    # Dominant Color Image:
+    # imwrite('/tmp/out.png', ia.viz())
+    # List structure for JSON:
+    print(ia.list())
