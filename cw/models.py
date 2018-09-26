4d3de2e36cb576fd317e7c5b4c4850fe9912f62e
Jon Stroop <jpstroop@gmail.com>
2018-09-26 19:05:55 -0400

    comment cleanup

--- cw/models.py
+++ cw/models.py
@@ -39,29 +39,15 @@ class _CWClass:
 @dataclass
 class ColorVolume(_CWClass):
     __slots__ = ('relative_volume', 'rgb')
-    # {
-    #   "relative_volume": 0.18552781289506953,
-    #   "rgb": [107,90,63]
-    # }
     relative_volume: float
     rgb: List[int] # May want this to be an object: Color(r,g,b)
 
 @dataclass
 class Image(_CWClass):
-    __slots__ = ('colors', 'palette_image', 'source_image') #, '_color_variance')
-    # {
-    #   "colors": [
-    #     { "relative_volume": 0.7999939143135346, "rgb": [98, 107, 103] },
-    #     { "relative_volume": 0.10508763388510224, "rgb": [53, 25, 22] },
-    #     { "relative_volume": 0.0949184518013632, "rgb": [186, 166, 138] }
-    #   ],
-    #   "palette_image": "0038.png",
-    #   "source_image": "0038.jpg"
-    # }
+    __slots__ = ('colors', 'palette_image', 'source_image')
     colors: List[ColorVolume]
     palette_image: str
     source_image: str
-    # _color_variance: float = None
 
     @property
     def color_variance(self):
@@ -75,31 +61,13 @@ class Image(_CWClass):
 
 @dataclass
 class ImageSet(_CWClass):
-    __slots__ = ('colors',)
-    # TODO: also make this a subclass of list, or else find a way to delegate
-    # all list-like methods to .images
-    # [
-    #   {
-    #     "colors": [
-    #       { "relative_volume": 0.7175252844500632, "rgb": [138, 118, 85] },
-    #       // ...
-    #     ],
-    #     "palette_image": "0001.png",
-    #     "source_image": "0001.jpg"
-    #   },
-    #   {
-    #     "colors": [
-    #       { "relative_volume": 0.5946930555555555, "rgb": [203, 173, 113] },
-    #       // ...
-    #     ],
-    #     "palette_image": "0002.png",
-    #     "source_image": "0002.jpg"
-    #   },
-    #   ...
-    # ]
+    __slots__ = ('images',)
+    # TODO: also make this a subclass of list, or else find a way to
+    # delegate all list-like methods to .images
     images: List[Image]
 
-    # We could probably make the superclass handle lists; hack in the meantime:
+    # We could probably make the superclass handle lists; hack in the
+    # meantime. See above also.
     def to_jsons(self):
         return dumps(self.to_dict()['images'])
 
@@ -114,10 +82,6 @@ class ImageSet(_CWClass):
             d = load(f)
             set = ImageSet.from_dict_or_list(d)
         return set
-
-# re: dataclasses:
-# https://realpython.com/python-data-classes/
-# https://docs.python.org/3/library/dataclasses.html
 
 if __name__ == "__main__":
     set = ImageSet.from_file('/Users/jstroop/workspace/colorweight/data.json')
